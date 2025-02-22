8d40aef053f8db1d545f0be7bf33a3f45cacbca5
amintester <amin.tester110@gmail.com>
2025-02-22 15:57:41 +0330

    remove Authentication

--- BlazorWasmClient/Program.cs
+++ BlazorWasmClient/Program.cs
@@ -14,12 +14,12 @@ public class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-        builder.Services.AddOidcAuthentication(options =>
-        {
-            // Configure your authentication provider options here.
-            // For more information, see https://aka.ms/blazor-standalone-auth
-            builder.Configuration.Bind("Local", options.ProviderOptions);
-        });
+        //builder.Services.AddOidcAuthentication(options =>
+        //{
+        //    // Configure your authentication provider options here.
+        //    // For more information, see https://aka.ms/blazor-standalone-auth
+        //    builder.Configuration.Bind("Local", options.ProviderOptions);
+        //});
 
 
         //my
