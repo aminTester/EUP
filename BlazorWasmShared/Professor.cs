2cd51f4b9559cc776419ae41929dad5a2b12e216
amintester <amin.tester110@gmail.com>
2025-02-24 13:52:47 +0330

    set <nullable> to true in all proj

--- BlazorWasmShared/Professor.cs
+++ BlazorWasmShared/Professor.cs
@@ -5,9 +5,7 @@ namespace BlazorWasmShared.Models
     public class Professor
     {
         public int Id { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Name { get; set; }
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Department { get; set; }
 
         public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
