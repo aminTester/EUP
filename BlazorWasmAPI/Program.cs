6058576f262acac2e0cc80f6debaf1fea9269694
amintester <amin.tester110@gmail.com>
2025-02-24 10:48:33 +0330

    set to Run migration automatically

--- BlazorWasmAPI/Program.cs
+++ BlazorWasmAPI/Program.cs
@@ -74,6 +74,17 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+
+
+//run migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
