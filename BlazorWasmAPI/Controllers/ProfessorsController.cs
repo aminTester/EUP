8deb6dd8bdb29aa930ff8e04f131d850f3edd7da
amintester <amin.tester110@gmail.com>
2025-02-24 17:39:49 +0330

    add migration for changing prof model

--- BlazorWasmAPI/Controllers/ProfessorsController.cs
+++ BlazorWasmAPI/Controllers/ProfessorsController.cs
@@ -17,7 +17,7 @@ namespace BlazorWasmAPI.Controllers
             _context = context;
         }
 
-        [HttpGet("country/{country}")]
+        [HttpGet("{country}")]
         public async Task<ActionResult<IEnumerable<Professor>>> GetProfessorsByCountry(string country)
         {
             var professors = await _context.Professors.Where(p => p.Country == Enum.Parse<CountryCode>(country)).ToListAsync();
--- BlazorWasmAPI/Migrations/20250224140901_ChangrProfModel.Designer.cs
+++ BlazorWasmAPI/Migrations/20250224140901_ChangrProfModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorWasmAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlazorWasmAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250224140901_ChangrProfModel")]
+    partial class ChangrProfModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
