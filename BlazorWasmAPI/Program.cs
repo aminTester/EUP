b2519fc0b3af215419d111a304c4db3034b1b96a
amintester <amin.tester110@gmail.com>
2025-02-26 12:34:48 +0330

    .

--- BlazorWasmAPI/Program.cs
+++ BlazorWasmAPI/Program.cs
@@ -33,8 +33,8 @@ builder.Services.AddCors(options =>
         {
             policy.WithOrigins(ClientBaseUrl)
                   .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
+                  .AllowAnyMethod();
+                  //.AllowCredentials();
         });
 });
 
